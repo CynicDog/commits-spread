a25292af0c053d2cbc9f38bf0fb72d1cdbb37a82
이은상 <kyrsang0801@inbrein.com>
2024-07-23 13:27:32 +0900

    Feat: integrated HashRouter for the routing deployment of GitHub Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Spread from "./components/Spread.jsx";
 import Network from "./components/Network.jsx";
@@ -7,9 +7,9 @@ function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/commits-spread" element={<Spread />} />
-                <Route path="/commits-spread/spread" element={<Spread />} />
-                <Route path="/commits-spread/network" element={<Network />} />
+                <Route path="/" element={<Spread />} />
+                <Route path="/spread" element={<Spread />} />
+                <Route path="/network" element={<Network />} />
             </Routes>
         </Router>
     );
