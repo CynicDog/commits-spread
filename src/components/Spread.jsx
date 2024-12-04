ef31ab15b77f325cb09f872168cc89e461e15342
eunsang <thecynicdog0328@gmail.com>
2024-12-04 16:14:59 +0900

    Update Spread.jsx

--- src/components/Spread.jsx
+++ src/components/Spread.jsx
@@ -9,7 +9,7 @@ const Spread = () => {
         d3.json('https://raw.githubusercontent.com/CynicDog/commits-spread/main/commit_history.json')
             .then(data => {
 
-                const recentData = data.slice(data.length - 70, data.length);
+                const recentData = data.slice(data.length - 140, data.length);
 
                 setData(recentData);
                 setLoading(false);
